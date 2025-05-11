401078a1879a6dd7f0f126e3625fe78ec36f5207
Mario Melo <130798353+mariomedev@users.noreply.github.com>
2025-05-11 10:23:36 -0500

    FT: add SplashScreen widget with animation and navigation to home

---
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../shared/shared.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animationScale;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 3,
+      ),
+    );
+    _animationScale = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOutQuad,
+      //easeOutBounce
+    );
+    _animationScale.addListener(() {
+      if (_animationScale.isCompleted) {
+        context.push('/home');
+      }
+    });
+
+    _controller.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Brackground(
+      child: ScaleTransition(
+        scale: _animationScale,
+        child: Center(
+          child: Image.asset(
+            'assets/img/flutter-mario.png',
+            height: 400,
+            width: 400,
+          ),
+        ),
+      ),
+    );
+  }
+}
