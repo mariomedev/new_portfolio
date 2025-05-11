dd7ff713020acd7e88d41eadfa3cca5dd37dc907
Mario Melo <130798353+mariomedev@users.noreply.github.com>
2025-05-11 10:29:55 -0500

    RF: refactor app router to set initial location to splash screen and simplify route builders

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,61 +1,29 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../presentation/screens/screens.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/splash',
   routes: [
     GoRoute(
+      path: '/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
       path: '/home',
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          key: state.pageKey,
-          child: const HomeScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: CurveTween(
-                curve: Curves.easeInOutCirc,
-              ).animate(animation),
-              child: child,
-            );
-          },
-        );
-      },
+      builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/about-me',
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          key: state.pageKey,
-          child: const AboutMeScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: CurveTween(
-                curve: Curves.easeInOutCirc,
-              ).animate(animation),
-              child: child,
-            );
-          },
-        );
-      },
+      builder: (context, state) => const AboutMeScreen(),
     ),
     GoRoute(
       path: '/projects',
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          key: state.pageKey,
-          child: const ProjectsScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: CurveTween(
-                curve: Curves.easeInOutCirc,
-              ).animate(animation),
-              child: child,
-            );
-          },
-        );
-      },
+      builder: (context, state) => const ProjectsScreen(),
     ),
+    GoRoute(
+      path: '/contact-me',
+      builder: (context, state) => const ContactScreen(),
+    )
   ],
 );
